d96fe5a1f6c96d3e8a84fc4a590f374eec506bc5
stevens-murillo <estevenmurillo@gmail.com>
2025-10-24 12:22:28 -0500

    Creación de paginas y publicaciones

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -12,5 +12,5 @@ export function getStrapiMedia(url: string | null) {
 export function formatDate(dateString: string) {
   const date = new Date(dateString);
   const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-  return date.toLocaleDateString('en-US', options);
+  return date.toLocaleDateString('es-CO', options);
 }
